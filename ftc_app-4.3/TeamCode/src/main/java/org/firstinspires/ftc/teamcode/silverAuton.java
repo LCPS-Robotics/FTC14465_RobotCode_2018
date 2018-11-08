16505c81873da90a8f42141e435787bdc987bac7
Mr. Siefen <43705292+siefech@users.noreply.github.com>
2018-11-07 19:18:06 -0500

    silvr auton template

--- ftc_app-4.3/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/silverAuton.java
+++ ftc_app-4.3/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/silverAuton.java
@@ -1,12 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name="Silver Auto", group="14465")
-public class silverAuton {
+public class silverAuton extends LinearOpMode {
     //lFDrive Motors
     private DcMotor lFDrive = null;
     private DcMotor lRDrive = null;
@@ -75,5 +76,113 @@ public class silverAuton {
 
         waitForStart();
 
+
+    }
+
+    public void bucketControl(double speed, int position, boolean scooperOn, boolean in){
+        if (scooperOn){
+            //scooper on
+            if (in){
+                //scoop in
+                bucketFlaps.setPower(.5);
+            }
+            else{
+                //scoop out
+                bucketFlaps.setPower(-.5);
+            }
+            bucketAngle.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            bucketAngle.setTargetPosition(position);
+            bucketAngle.setPower(speed);
+            while(bucketAngle.isBusy()){
+
+            }
+            bucketAngle.setPower(0);
+        }
+        else{
+            bucketFlaps.setPower(0);
+            bucketAngle.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            bucketAngle.setTargetPosition(position);
+            bucketAngle.setPower(speed);
+            while(bucketAngle.isBusy()){
+
+            }
+            bucketAngle.setPower(0);
+        }
+
+
+
+    }
+
+    public void armControl(double speed, int position, boolean Up){
+        upArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        lowArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        if (Up){
+            //if up arm
+            upArm.setTargetPosition(position);
+            upArm.setPower(speed);
+        }
+        else{
+            //if low arm
+            lowArm.setTargetPosition(position);
+            lowArm.setPower(speed);
+        }
+        while(lowArm.isBusy() || upArm.isBusy()){
+
+        }
+        lowArm.setPower(0);
+        upArm.setPower(0);
+
+        lowArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        upArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void climbDown(){
+        lift1.setPosition(.82);
+        lift2.setPosition(.82);
+    }
+
+    public void encoderDrive(double speed, double leftInches, double rightInches, double timeoutS) {
+        int newLeftTarget;
+        int newRightTarget;
+
+        if (opModeIsActive()) {
+            newLeftTarget = lFDrive.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH);
+            newRightTarget = rFDrive.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
+
+            lFDrive.setTargetPosition(newLeftTarget);
+            rFDrive.setTargetPosition(newRightTarget);
+            lRDrive.setTargetPosition(newLeftTarget);
+            rRDrive.setTargetPosition(newRightTarget);
+
+            lFDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rFDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            lRDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rRDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            runTime.reset();
+            lFDrive.setPower(Math.abs(speed));
+            rFDrive.setPower(Math.abs(speed));
+            lRDrive.setPower(Math.abs(speed));
+            rRDrive.setPower(Math.abs(speed));
+
+            while (opModeIsActive() &&
+                    (runTime.seconds() < timeoutS) &&
+                    (lFDrive.isBusy() && rFDrive.isBusy())) {
+                telemetry.addData("Path1", "Running to %7d: %7d", newLeftTarget, newRightTarget);
+                telemetry.addData("Path2", "Running at %7d :%7d", lFDrive.getCurrentPosition(), rFDrive.getCurrentPosition());
+                telemetry.update();
+            }
+
+            lFDrive.setPower(0);
+            rFDrive.setPower(0);
+            lRDrive.setPower(0);
+            rRDrive.setPower(0);
+
+            lFDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            rFDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            lRDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            rRDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
     }
 }
