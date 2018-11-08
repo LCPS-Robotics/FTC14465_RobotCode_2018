0a2f73b4081d1005935ad68b68a7f74838427057
Mr. Siefen <43705292+siefech@users.noreply.github.com>
2018-11-07 19:14:24 -0500

    Silver Auton

--- ftc_app-4.3/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/silverAuton.java
+++ ftc_app-4.3/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/silverAuton.java
@@ -1,4 +1,79 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name="Silver Auto", group="14465")
 public class silverAuton {
+    //lFDrive Motors
+    private DcMotor lFDrive = null;
+    private DcMotor lRDrive = null;
+
+    //rFDrive Motors
+    private DcMotor rFDrive = null;
+    private DcMotor rRDrive = null;
+
+    //Arm Motors
+    private DcMotor upArm = null;
+    private DcMotor lowArm = null;
+
+    //Bucket Motors
+    private DcMotor bucketAngle = null;
+    private DcMotor bucketFlaps = null;
+
+    //Climbing Servos
+    private Servo lift1 = null;
+    private Servo lift2 = null;
+
+    private ElapsedTime runTime = new ElapsedTime();
+
+    static final double WHEEL_DIAMETER_INCHES = 4.0;
+    static final double COUNTS_PER_MOTOR_REV = 1440;
+    static final double DRIVE_GEAR_REDUCTION = 1.0;
+
+    static final double DRIVE_SPEED = 0.6;
+    static final double TURN_SPEED = 0.5;
+    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        lFDrive = hardwareMap.get(DcMotor.class, "LFDrive");
+        lRDrive = hardwareMap.get(DcMotor.class, "LRDrive");
+
+        rFDrive = hardwareMap.get(DcMotor.class, "RFDrive");
+        rRDrive = hardwareMap.get(DcMotor.class, "RRDrive");
+
+        upArm = hardwareMap.get(DcMotor.class, "UpArm");
+        lowArm = hardwareMap.get(DcMotor.class, "LowArm");
+
+        bucketAngle = hardwareMap.get(DcMotor.class, "bucketAngle");
+        bucketFlaps = hardwareMap.get(DcMotor.class, "bucketFlap");
+
+        lift1 = hardwareMap.get(Servo.class, "lift1");
+        lift2 = hardwareMap.get(Servo.class, "lift2");
+
+        rFDrive.setDirection(DcMotor.Direction.REVERSE);
+        rRDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        telemetry.addData("Status", "Resetting Encoders");
+        telemetry.update();
+
+        lFDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        lRDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rFDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rRDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        lFDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        lRDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rFDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rRDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        telemetry.addData("Path0", "Starting at %7d : %7d", lFDrive.getCurrentPosition(), rFDrive.getCurrentPosition());
+        telemetry.update();
+
+        waitForStart();
+
+    }
 }
