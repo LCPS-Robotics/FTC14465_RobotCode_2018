c102d1ddf880484e411feae8954f1b48ec543f89
Mr. Siefen <43705292+siefech@users.noreply.github.com>
2018-11-17 13:59:57 -0500

    Update ftc_app-4.3/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sensorTest.java

--- ftc_app-4.3/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sensorTest.java
+++ ftc_app-4.3/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sensorTest.java
@@ -2,12 +2,17 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 
 @TeleOp(name = "Sensor test", group = "Sensors")
 public class sensorTest extends LinearOpMode {
 
+    ColorSensor sensorColor;
+    DistanceSensor sensorDistance;
+
     @Override
     public void runOpMode() throws InterruptedException {
-        
+
     }
 }
