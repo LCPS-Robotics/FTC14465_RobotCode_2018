eb51ef4427856446378824c1e6e5c835a4ce3b71
Mr. Siefen <43705292+siefech@users.noreply.github.com>
2018-11-26 19:32:55 -0500

    Update servoTest.java

--- ftc_app-4.3/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servoTest.java
+++ ftc_app-4.3/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servoTest.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -12,11 +11,33 @@ public class servoTest extends OpMode {
 
     @Override
     public void init() {
+        telemetry.addData("Servo Angle", bucket.getPosition());
+        bucket = hardwareMap.get(Servo.class, "bucketServo");
+    }
 
+    @Override
+    public void start(){
+        bucket.setPosition(.25);
     }
 
     @Override
     public void loop() {
+        telemetry.addData("Servo Angle", bucket.getPosition());
 
+        if(gamepad1.right_bumper){
+            bucket.setPosition(.25);
+        }
+        else{
+
+        }
+
+        if(gamepad1.left_bumper){
+            bucket.setPosition(1);
+        }
+        else {
+
+        }
+
+        telemetry.update();
     }
 }
