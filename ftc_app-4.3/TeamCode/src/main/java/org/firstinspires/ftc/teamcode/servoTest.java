f4380fefb9690a59f2cd505cd17cf535ef37347e
Mr. Siefen <43705292+siefech@users.noreply.github.com>
2018-11-26 19:11:17 -0500

    Update servoTest.java

--- ftc_app-4.3/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servoTest.java
+++ ftc_app-4.3/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servoTest.java
@@ -1,11 +1,22 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
 
-public class servoTest extends LinearOpMode {
+@TeleOp(name = "Servo Test", group = "Sensors")
+public class servoTest extends OpMode {
+
+    private Servo bucket = null;
 
     @Override
-    public void runOpMode() throws InterruptedException {
-        
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
     }
 }
