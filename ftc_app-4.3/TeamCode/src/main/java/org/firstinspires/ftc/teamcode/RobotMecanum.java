d189aca466a0cff49983c2ba7e5d22fb7c064955
Mr. Siefen <43705292+siefech@users.noreply.github.com>
2018-11-29 18:52:01 -0500

    Update RobotMecanum.java

--- ftc_app-4.3/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotMecanum.java
+++ ftc_app-4.3/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotMecanum.java
@@ -2,12 +2,23 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name="Mecanum Robot", group="Teleop")
 public class RobotMecanum extends OpMode {
+
+    private DcMotor lFDrive = null;
+    private DcMotor lRDrive = null;
+    private DcMotor rFDrive = null;
+    private DcMotor rRDrive = null;
+
     @Override
     public void init() {
-
+        lFDrive = hardwareMap.get(DcMotor.class, "LFDrive");
+        lRDrive = hardwareMap.get(DcMotor.class, "LRDrive");
+        rFDrive = hardwareMap.get(DcMotor.class, "RFDrive");
+        rRDrive = hardwareMap.get(DcMotor.class, "RRDrive");
+        
     }
 
     @Override
